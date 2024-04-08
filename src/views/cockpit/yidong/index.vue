bddd7a0c98dcc96071a682b847751e5fdd7512f1
杨宏超 <1925119849@qq.com>
2024-04-08 16:05:46 +0800

    🍃 update(图片上传组件): 目前放在 首页的index上

---
+++ src/views/cockpit/yidong/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cockpit">
+    <iframe src="https://cockpit.ycbrain.com/manager-cockpit/user" frameborder="0" class="box"></iframe>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+
+.box{
+    width: 100%;
+    height: 600px;
+}
+</style>
---
+++ src/views/union/msgs/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="union">
+    <h1>2222</h1>
+    <button v-hasPermi="['union:msgs:test']">11111</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
